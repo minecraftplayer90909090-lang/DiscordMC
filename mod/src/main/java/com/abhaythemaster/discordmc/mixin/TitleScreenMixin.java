4109cbdc33e8f975f8413d5667e64450c2ca1ca0
minecraftplayer909090 <minecraftplayer909090@gmail.com>
2026-06-25 16:15:15 +0530

    Fix TitleScreenMixin protected access

--- mod/src/main/java/com/abhaythemaster/discordmc/mixin/TitleScreenMixin.java
+++ mod/src/main/java/com/abhaythemaster/discordmc/mixin/TitleScreenMixin.java
@@ -1,6 +1,7 @@
 package com.abhaythemaster.discordmc.mixin;
 
 import com.abhaythemaster.discordmc.gui.DiscordMainScreen;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
@@ -10,14 +11,19 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class TitleScreenMixin {
+public abstract class TitleScreenMixin extends net.minecraft.client.gui.screen.Screen {
+
+    protected TitleScreenMixin() {
+        super(Text.literal(""));
+    }
+
     @Inject(at = @At("RETURN"), method = "init")
     private void addDiscordButton(CallbackInfo ci) {
-        TitleScreen screen = (TitleScreen)(Object)this;
-        screen.addDrawableChild(ButtonWidget.builder(
+        this.addDrawableChild(ButtonWidget.builder(
             Text.literal("§5⚡ Discord"),
-            btn -> screen.client.setScreen(new DiscordMainScreen(screen)))
-            .dimensions(screen.width / 2 - 100, screen.height / 4 + 120, 200, 20)
+            btn -> MinecraftClient.getInstance().setScreen(
+                new DiscordMainScreen(MinecraftClient.getInstance().currentScreen)))
+            .dimensions(this.width / 2 - 100, this.height / 4 + 120, 200, 20)
             .build());
     }
 }
